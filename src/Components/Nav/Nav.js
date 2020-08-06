f3e2b41460aeca3f0622926781f60e36f72c4f99
brady <bradyjbridges@gmail.com>
2020-08-06 17:48:25 -0500

    Remove default export and just export component so it can be tested

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import timberline_logo from '../../assets/timberline-const.png';
 import logo_25 from '../../assets/25+_timberline.png';
 
-const Nav = () => {
+export const Nav = () => {
   return (
     <header>
       <Link to="/">
-        <img id="timberline_logo" src={timberline_logo}/>
+        <img id="timberline_logo" src={timberline_logo} alt="timberline logo"/>
       </Link>
       <Link to="/about">
         <button id="about_nav_button" className="nav_button">About</button>
@@ -19,10 +19,8 @@ const Nav = () => {
         <button id="contact_us_nav_button" className="nav_button">Contact Us</button>
       </Link>
       <Link to="history">
-        <img id="logo_25" src={logo_25}/>
+        <img id="logo_25" src={logo_25} alt="logo"/>
       </Link>
     </header>
   )
 }
-
-export default Nav;
