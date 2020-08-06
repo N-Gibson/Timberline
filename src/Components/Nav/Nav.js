7990224a8206f27cb90b6dc930fbaa01b1dd1462
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 15:49:52 -0600

    Add id to nav buttons

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,13 +6,13 @@ export const Nav = () => {
     <header>
       <img src="../../resources/timberline-const.png"/>
       <Link to="/about">
-        <button className="about_nav_button">About</button>
+        <button id="about_nav_button" className="nav_button">About</button>
       </Link>
       <Link to="/projects">
-        <button className="projects_nav_button">Projects</button>
+        <button id="projects_nav_button" className="nav_button">Projects</button>
       </Link>
       <Link to="/contact-us">
-        <button className="contact_us_nav_button">Contact Us</button>
+        <button id="contact_us_nav_button" className="nav_button">Contact Us</button>
       </Link>
       <img src="../../resources/25+_timberline.png"/>
     </header>
