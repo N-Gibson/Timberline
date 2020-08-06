c89b1896353782ceb09985263aecb51d57b760c1
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 16:27:24 -0600

    Fix logo renders

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import timberline_logo from '../../assets/timberline-const.png';
+import logo_25 from '../../assets/25+_timberline.png';
 
 const Nav = () => {
   return (
     <header>
       <Link to="/">
-        <img id="timberline_logo" src="../../../assets/timberline-const.png"/>
+        <img id="timberline_logo" src={timberline_logo}/>
       </Link>
       <Link to="/about">
         <button id="about_nav_button" className="nav_button">About</button>
@@ -17,7 +19,7 @@ const Nav = () => {
         <button id="contact_us_nav_button" className="nav_button">Contact Us</button>
       </Link>
       <Link to="history">
-        <img id="logo_25" src="../../../assets/25+_timberline.png"/>
+        <img id="logo_25" src={logo_25}/>
       </Link>
     </header>
   )
