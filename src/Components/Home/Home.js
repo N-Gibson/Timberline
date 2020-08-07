fa8eead212ef91cb166b45d6e3399aa60e90a7eb
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 20:55:03 -0600

    Add narratives and images to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
 import venadoFront from '../../assets/Venado_front.jpg';
 import purpleAster from '../../assets/Purple_Aster.jpg';
+import profile from '../../assets/profile.jpg';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 export class Home extends Component {
@@ -43,6 +44,18 @@ export class Home extends Component {
           autoPlay={this.state.autoPlay}
           slideInterval={this.state.slideInterval}
         />
+        <div className="center_main">
+          <div className="rrr_content">
+            <h2>Raise, Repair, Remodel</h2>
+            <p>Timberline Inc. is a locally owned and operated business in Santa Fe, New Mexico and surrounding areas since 1992. Specializing in New Mexican style builds and remodels, Timberline can do it all. From the preservation of original homes dating back to the mid 1800's to brand new homes and everything in between! Award winning Timberline Inc. can help with repairs, remodels or raising a new project from the ground up.</p>
+          </div>
+          <img src={purpleAster}/>
+        </div>
+        <div className="bottom_main">
+          <h2>Meet the Owner</h2>
+          <p>Stephen Gibson, owner and founder of Timberline Inc. has been working in the Santa Fe community for over 35 years and has received 11 awards in the Santa Fe area Home Builders Assocation's - A Parade of Homes. Steve's passion is crafting quality solutions for your construction needs.</p>
+          <img src={profile}/>
+        </div>
       </section>
     )
   }
