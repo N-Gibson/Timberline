2d7e7764974ca4c46928530b7f84f3b1c3ee1a75
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 18:14:11 -0600

    Update home to have snapshot test

--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -3,7 +3,9 @@ import { shallow } from 'enzyme';
 import './Home';
 
 describe('Home', () => {
-  it('should have a passing test for now', () => {
-    expect(true).toEqual(true);
+  const wrapper = shallow(<Home />);
+  
+  it('Home component should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
