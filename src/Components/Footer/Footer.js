3790a26fe06f20067c6c3550239b809eeb436162
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 21:07:06 -0600

    Add footer contetn

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Contact Us</p>
+      <p>(505)-470-5446</p>
+      <p>timberlineinc.steve@gmail.com</p>
+    </footer>
+  )
+}
