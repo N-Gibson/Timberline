bcc80371bbb700aa1b37546099104e002c30833e
Noah Gibson <noah.gibson99@gmail.com>
2020-08-06 21:08:15 -0600

    Add footer to app render

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import { Nav } from '../src/Components/Nav/Nav';
 import { Home } from '../src/Components/Home/Home';
+import { Footer } from '../src/Components/Footer/Footer'; 
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Route exact path="/" render={() => <Home />}/>
+      <Footer />
     </div>
   );
 }
