072faef67cfa2937c9e195fc6c924b6fc1954d03
Noah Gibson <noah.gibson99@gmail.com>
2020-08-11 21:43:10 -0600

    Change email to email href

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,9 @@ export const Footer = () => {
   return (
     <footer>
       <p className="footer_content">Contact Us</p>
-      <p className="footer_content">(505)-470-5446</p>
-      <p className="footer_content">timberlineinc.steve@gmail.com</p>
+      <p className="footer_content">Phone: (505)-470-5446</p>
+      <a href="mailto: timberlineinc.steve@gmail.com" >Email: timberlineinc.steve@gmal.com</a>
+      {/* <p className="footer_content">timberlineinc.steve@gmail.com</p> */}
     </footer>
   )
 }
