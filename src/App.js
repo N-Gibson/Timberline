b57034fafe4f3c6b51a77cd706f8757d3b34838e
brady <bradyjbridges@gmail.com>
2020-08-17 21:39:34 -0500

    Relolve conflict with merge, add route to about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
-import { Nav } from '../src/Components/Nav/Nav';
-import { Home } from '../src/Components/Home/Home';
-import { Footer } from '../src/Components/Footer/Footer'; 
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import { Nav } from "../src/Components/Nav/Nav";
+import { Footer } from "../src/Components/Footer/Footer";
+import Home from "../src/Components/Home/Home";
+import About from "../src/Components/About/About";
 
 function App() {
   return (
     <main className="App">
-      <Nav active="home"/>
-      <Route exact path="/" render={() => <Home />}/>
+      <Nav active="home" />
+      <Route exact path="/" render={() => <Home />} />
+      <Route path="/about" render={() => <About />} />
       <Footer />
     </main>
   );
