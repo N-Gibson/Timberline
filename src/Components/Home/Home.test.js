ae9335522b9ec9e6a2ba4466ee23243e3f6535a6
brady <bradyjbridges@gmail.com>
2020-08-17 21:40:34 -0500

    Update test after changing to default export

--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Home } from './Home';
+import React from "react";
+import { shallow } from "enzyme";
+import Home from "./Home";
 
-describe('Home', () => {
+describe("Home", () => {
   const wrapper = shallow(<Home />);
-  
-  it('Home component should match snapshot', () => {
+
+  it("Home component should match snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
