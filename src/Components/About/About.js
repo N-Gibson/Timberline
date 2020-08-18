70ad14f2c1471b91e11ecc20463137df9f9c52db
Noah Gibson <noah.gibson99@gmail.com>
2020-08-18 14:37:14 -0600

    Add about narratives

---
+++ src/Components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <section>
+      <article>
+        <p>
+          Timberline Inc. is not just about creating quality solutions to problems that arise throughout the duration of the home owning timeline, it is also about creating an extended family within the community. Compassion, empathy, understanding are just some of the values that sit at the very core of Timberline Inc. Steve Gibson is a creative problem solver that will work with you to find the perfect solution. If you would like to work with him, please view some of the items on his project bucket list!
+        </p>
+      </article>
+      <article>
+        <h2>Bucket List</h2>
+        <ul>
+          <li>Work on a home from the 1800's √</li>
+          <li>Build a cabin in Alaska</li>
+          <li>Design and build a liveable tree house</li>
+        </ul>
+      </article>
+    </section>
+  )
+}
