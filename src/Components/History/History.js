d4e071dd27e4de5789cc68a87f745e76068e5d20
Noah Gibson <noah.gibson99@gmail.com>
2020-08-18 14:38:43 -0600

    Update classes in history component

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -4,15 +4,15 @@ import "./History.scss";
 
 export default function History() {
   return (
-    <section className="about">
-      <article className="about_article">
-        <p className="about_article_text">
+    <section className="history">
+      <article className="history_article">
+        <p className="history_article_text">
           Timberline Inc. was founded in 1995 with the goals of creating quality
           solutions in the housing market. It has evolved into a company that
           can do a bit of everything. Etc... this narrative will need some work.
         </p>
         <img
-          className="about_main_image"
+          className="history_main_image"
           src={Hallway}
           alt="hallway in house"
         />
