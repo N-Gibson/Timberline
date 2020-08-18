4b1d30415cbf1c2a5452c0d563d81af69d75ec62
Noah Gibson <noah.gibson99@gmail.com>
2020-08-18 15:05:57 -0600

    Add styling on about component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.scss';
 
-export const About = () => {
+export default function About() {
   return (
     <section className="about">
       <article className="about_article">
@@ -9,7 +9,7 @@ export const About = () => {
           Timberline Inc. is not just about creating quality solutions to problems that arise throughout the duration of the home owning timeline, it is also about creating an extended family within the community. Compassion, empathy, understanding are just some of the values that sit at the very core of Timberline Inc. Steve Gibson is a creative problem solver that will work with you to find the perfect solution. If you would like to work with him, please view some of the items on his project bucket list!
         </p>
       </article>
-      <article className="about_article">
+      <article className="about_article about_article_bucket_list">
         <h2 className="bucket_list_title">Bucket List</h2>
         <ul className="bucket_list_ul">
           <li className="bucket_list_li">Work on a home from the 1800's √</li>
