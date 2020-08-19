d93adae3c442916bc2f6ccb3e6f8a69e8a700cfe
Noah Gibson <noah.gibson99@gmail.com>
2020-08-19 12:27:42 -0600

    Add image imports to projects

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Projects.scss';
+import purpleAster from '../../assets/Purple_Aster.jpg';
+import vendaoFront from '../../assets/Venado_front.jpg';
+import venadoPorch from '../../assets/Venado_Porch.jpg';
+
+export const Projects = () => {
+  return (
+    <section className="projects">
+      <img src={purpleAster}>Purple Aster Spec Home</img>
+    </section>
+  )
+}
