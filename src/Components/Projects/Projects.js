b0bbb5860abbd39fbd36694bee52fe2f99e1df80
Noah Gibson <noah.gibson99@gmail.com>
2020-08-21 16:50:50 -0600

    Add layout to projects tab

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Layout from '../Layout/Layout';
 import './Projects.scss';
 import purpleAster from '../../assets/Purple_Aster.jpg';
 import venadoFront from '../../assets/Venado_front.jpg';
@@ -6,14 +7,16 @@ import venadoKitchen from '../../assets/Venado_Kitchen.jpg';
 
 export const Projects = () => {
   return (
-    <section className="projects">
-      <img id="purple_aster" src={purpleAster} />
-      <label htmlFor="purple_aster">Purple Aster Spec Home</label>
-      <img id="venado_front" src={venadoFront} />
-      <label htmlFor="venado_front">Venado Spec Home</label>
-      <img id="venado_kitchen" src={venadoKitchen}/>
-      <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
-    </section>
+    <Layout active="projects">
+      <section className="projects">
+        <img id="purple_aster" src={purpleAster} />
+        <label htmlFor="purple_aster">Purple Aster Spec Home</label>
+        <img id="venado_front" src={venadoFront} />
+        <label htmlFor="venado_front">Venado Spec Home</label>
+        <img id="venado_kitchen" src={venadoKitchen}/>
+        <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
+      </section>
+    </Layout>
   )
 }
 
