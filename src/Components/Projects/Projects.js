66af014d50915b61246c5907d0c3ca544eb4106d
Noah Gibson <noah.gibson99@gmail.com>
2020-08-29 16:25:12 -0600

    Add base style for projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
 import './Projects.scss';
+import { Link } from 'react-router-dom';
 import purpleAster from '../../assets/Purple_Aster.jpg';
 import venadoFront from '../../assets/Venado_front.jpg';
 import venadoKitchen from '../../assets/Venado_Kitchen.jpg';
@@ -9,12 +10,18 @@ export const Projects = () => {
   return (
     <Layout active="projects">
       <section className="projects">
-        <img id="purple_aster" src={purpleAster} />
-        <label htmlFor="purple_aster">Purple Aster Spec Home</label>
-        <img id="venado_front" src={venadoFront} />
-        <label htmlFor="venado_front">Venado Spec Home</label>
-        <img id="venado_kitchen" src={venadoKitchen}/>
-        <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
+        <div className="project_container">
+          <img id="purple_aster" className="projects_photos" src={purpleAster} />
+          <label htmlFor="purple_aster">Purple Aster Spec Home</label>
+        </div>
+        <div className="project_container">
+          <img id="venado_front" className="projects_photos" src={venadoFront} />
+          <label htmlFor="venado_front">Venado Spec Home</label>
+        </div>
+        <div className="project_container">
+          <img id="venado_kitchen" className="projects_photos" src={venadoKitchen}/>
+          <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
+        </div>
       </section>
     </Layout>
   )
