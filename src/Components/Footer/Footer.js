d895d2fa9e8d280a55dbe7b4d0090a3e574fe7c8
Noah Gibson <noah.gibson99@gmail.com>
2020-08-29 16:42:08 -0600

    Look into footer style within projects

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,9 @@ export const Footer = () => {
     <footer>
       <p className="footer_content">Contact Us</p>
       <p className="footer_content">Phone: (505)-470-5446</p>
-      <a href="mailto: timberlineinc.steve@gmail.com" >Email: timberlineinc.steve@gmal.com</a>
+      <div className="footer_content">
+        <a href="mailto: timberlineinc.steve@gmail.com" >Email: timberlineinc.steve@gmal.com</a>
+      </div>
       {/* <p className="footer_content">timberlineinc.steve@gmail.com</p> */}
     </footer>
   )
