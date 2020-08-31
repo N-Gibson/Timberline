3949898f3148993ea8034c71e135ef961e68adac
brady <bradyjbridges@gmail.com>
2020-08-31 18:42:00 -0500

    Remove commented out route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
-import { Nav } from "./Components/Nav/Nav";
-import { Footer } from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import History from "./Components/History/History";
@@ -14,7 +12,6 @@ function App() {
       <Route exact path="/" render={() => <Home />} />
       <Route exact path="/about" render={() => <About />} />
       <Route path="/projects" render={() => <Projects />} />
-      {/* <Route path="/projects/:id" render={() => <Project/>} /> */}
       <Route path="/contact-us" render={() => <Home />} />
       <Route path="/history" render={() => <History />} />
     </main>
