46beb4e5c7a17be10f23bc9df89dd5c35d750ba0
Noah Gibson <noah.gibson99@gmail.com>
2021-01-01 17:41:22 -0700

    Update imports in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.css";
-import { Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
-import History from "./Components/History/History";
-import Projects from "./Components/Projects/Projects";
-import ContactUs from "./Components/ContactUs/ContactUs";
+import React from 'react'
+import './App.css'
+import { Route } from 'react-router-dom'
+import { Home } from '@timberline/Components/Home'
+import { About } from '@timberline/Components/About'
+import { History } from '@timberline/Components/History'
+import { Projects } from '@timberline/Components/Projects'
+import { ContactUs } from '@timberline/Components/ContactUs'
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Route path="/contact-us" render={() => <ContactUs />} />
       <Route path="/history" render={() => <History />} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
