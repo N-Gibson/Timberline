1f72cc70ea86ec5bfe3193a21979098164725781
Noah Gibson <noah.gibson99@gmail.com>
2021-01-03 14:04:57 -0700

    Update contact dialog

--- src/@timberline/Components/ContactUs/index.tsx
+++ src/@timberline/Components/ContactUs/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ContactUs.scss'
 import { Layout } from '../Layout'
-import { ContactForm } from '@timberline/Components/ContactForm'
+import { ContactForm } from '@timberline/Components'
 
 const ContactUs = () => {
   return (
@@ -9,15 +9,13 @@ const ContactUs = () => {
       <div className="contactUs_container">
         <section className="contactUs">
           <article>
-            <h3>Interested in remodeling your home?</h3>
+            <h3>Interested in having some work done?</h3>
             <h2>Get in touch with us for a free quote!</h2>
             <p>
-              Eiusmod quis adipisicing irure laborum sint excepteur aute laboris
-              aute elit incididunt cupidatat nulla proident. Cillum officia
-              cillum do excepteur non laboris cupidatat enim tempor est fugiat
-              cupidatat anim. Cupidatat labore nulla pariatur qui. Dolor
-              adipisicing est consequat et non officia proident minim duis sunt
-              qui voluptate.
+              At Timberline Inc., we take pride in customer service above all
+              else! Reach out to us with your basic contact information as well
+              as a brief description of the work you need done. We will be in
+              touch soon, thank you!
             </p>
           </article>
           <ContactForm />
