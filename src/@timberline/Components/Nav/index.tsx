fdde1c41995e968df3c106ff1575033de001337f
Noah Gibson <noah.gibson99@gmail.com>
2021-01-03 15:26:19 -0700

    Remove unused use state

--- src/@timberline/Components/Nav/index.tsx
+++ src/@timberline/Components/Nav/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.scss'
 import timberline_logo from '../../assets/timberline-const.png'
