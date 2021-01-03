bad05502711ae2f766d72f1bdbe0d4eb5d81e924
Noah Gibson <noah.gibson99@gmail.com>
2021-01-03 15:26:05 -0700

    Update imports

--- src/@timberline/Components/Home/index.tsx
+++ src/@timberline/Components/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Layout } from '../Layout'
 import { Link } from 'react-router-dom'
-import purpleAster from '../../assets/Purple_Aster.jpg'
-import profile from '../../assets/profile.jpg'
+import purpleAster from '@timberline/assets/Purple_Aster.jpg'
+import profile from '@timberline/assets/profile.jpg'
 import 'react-image-gallery/styles/css/image-gallery.css'
 
 import './Home.scss'
