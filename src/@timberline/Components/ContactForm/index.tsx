fa5167e8bb02f1edd982ca06df954731dc25b2b2
Noah Gibson <noah.gibson99@gmail.com>
2021-01-10 16:53:03 -0700

    Debug contact form

--- src/@timberline/Components/ContactForm/index.tsx
+++ src/@timberline/Components/ContactForm/index.tsx
@@ -44,6 +44,11 @@ const ContactForm = () => {
         notify.success('Work inquiry successfully sent!')
       })
       .catch(() => {
+        console.log(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+          process.env.REACT_APP_EMAILJS_USER_ID,
+        )
         notify.error('There was an error sending your work inquiry.')
       })
 
