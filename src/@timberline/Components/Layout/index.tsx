cb22491aae7decea1abc09dcd44854ce06aa4545
Noah Gibson <noah.gibson99@gmail.com>
2021-01-12 22:45:51 -0700

    Add props to layout component

--- src/@timberline/Components/Layout/index.tsx
+++ src/@timberline/Components/Layout/index.tsx
@@ -5,12 +5,13 @@ import { Footer } from '../Footer'
 interface LayoutProps {
   children: any
   active: string
+  props: any
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, active }) => {
+const Layout: React.FC<LayoutProps> = ({ children, active, props }) => {
   return (
     <>
-      <Nav active={active} />
+      <Nav props={props} active={active} />
       <main>{children}</main>
       <Footer />
     </>
