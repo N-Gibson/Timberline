8eda67c6f611d22bd49c1069cada7120e9ca8388
Noah Gibson <noah.gibson99@gmail.com>
2021-01-23 14:27:25 -0700

    Remove layout from contact us component

--- src/@timberline/Components/ContactUs/index.tsx
+++ src/@timberline/Components/ContactUs/index.tsx
@@ -1,32 +1,28 @@
 import React from 'react'
 
-import { ContactForm, Layout } from '@timberline/Components'
+import { ContactForm } from '@timberline/Components'
 
 import './ContactUs.scss'
 
-interface ContactUsProps {
-  props: any
-}
+interface ContactUsProps {}
 
-const ContactUs: React.FC<ContactUsProps> = (props) => {
+const ContactUs: React.FC<ContactUsProps> = () => {
   return (
-    <Layout props={props} active="contact">
-      <div className="contactUs_container">
-        <section className="contactUs">
-          <article>
-            <h3>Interested in having some work done?</h3>
-            <h2>Get in touch with us for a free quote!</h2>
-            <p>
-              At Timberline Inc., we take pride in customer service above all
-              else! Reach out to us with your basic contact information as well
-              as a brief description of the work you need done. We will be in
-              touch soon, thank you!
-            </p>
-          </article>
-          <ContactForm />
-        </section>
-      </div>
-    </Layout>
+    <div className="contactUs_container">
+      <section className="contactUs">
+        <article>
+          <h3>Interested in having some work done?</h3>
+          <h2>Get in touch with us for a free quote!</h2>
+          <p>
+            At Timberline Inc., we take pride in customer service above all
+            else! Reach out to us with your basic contact information as well as
+            a brief description of the work you need done. We will be in touch
+            soon, thank you!
+          </p>
+        </article>
+        <ContactForm />
+      </section>
+    </div>
   )
 }
 
