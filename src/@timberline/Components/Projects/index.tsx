a60fe8ff37193164fd1fc6aad575d7e586296fcd
Noah Gibson <noah.gibson99@gmail.com>
2021-01-23 14:28:32 -0700

    Remove layout from projects component

--- src/@timberline/Components/Projects/index.tsx
+++ src/@timberline/Components/Projects/index.tsx
@@ -1,56 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { Layout } from '@timberline/Components'
 import purpleAster from '@timberline/assets/Purple_Aster.jpg'
 import venadoFront from '@timberline/assets/Venado_front.jpg'
 import venadoKitchen from '@timberline/assets/Venado_Kitchen.jpg'
 
 import './Projects.scss'
 
-interface ProjectsProps {
-  props: any
-}
+interface ProjectsProps {}
 
-const Projects: React.FC<ProjectsProps> = (props) => {
+const Projects: React.FC<ProjectsProps> = () => {
   return (
-    <Layout props={props} active="projects">
-      <section className="projects">
-        <Link to="/projects/1">
-          <div className="project_container">
-            <img
-              id="purple_aster"
-              className="projects_photos"
-              src={purpleAster}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="purple_aster">Purple Aster Spec Home</label>
-          </div>
-        </Link>
-        <Link to="/projects/2">
-          <div className="project_container">
-            <img
-              id="venado_front"
-              className="projects_photos"
-              src={venadoFront}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="venado_front">Venado Spec Home</label>
-          </div>
-        </Link>
-        <Link to="/projects/3">
-          <div className="project_container">
-            <img
-              id="venado_kitchen"
-              className="projects_photos"
-              src={venadoKitchen}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
-          </div>
-        </Link>
-      </section>
-    </Layout>
+    <section className="projects">
+      <Link to="/projects/1">
+        <div className="project_container">
+          <img
+            id="purple_aster"
+            className="projects_photos"
+            src={purpleAster}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="purple_aster">Purple Aster Spec Home</label>
+        </div>
+      </Link>
+      <Link to="/projects/2">
+        <div className="project_container">
+          <img
+            id="venado_front"
+            className="projects_photos"
+            src={venadoFront}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="venado_front">Venado Spec Home</label>
+        </div>
+      </Link>
+      <Link to="/projects/3">
+        <div className="project_container">
+          <img
+            id="venado_kitchen"
+            className="projects_photos"
+            src={venadoKitchen}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
+        </div>
+      </Link>
+    </section>
   )
 }
 
