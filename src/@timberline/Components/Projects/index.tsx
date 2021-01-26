074c92837388cb9920f84fb7d5c7b5233bade54b
Noah Gibson <noah.gibson99@gmail.com>
2021-01-26 12:32:54 -0700

    Set up projects for future navigation

--- src/@timberline/Components/Projects/index.tsx
+++ src/@timberline/Components/Projects/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 import { Tooltip } from '@material-ui/core'
 
 import purpleAster from '@timberline/assets/Purple_Aster.jpg'
@@ -11,46 +11,50 @@ import './Projects.scss'
 interface ProjectsProps {}
 
 const Projects: React.FC<ProjectsProps> = () => {
+  const history = useHistory()
+
+  const handleClick = () => {}
+
   return (
     <section className="projects">
       <Tooltip title="Coming Soon!">
-        <Link to="/projects/1">
-          <div className="project_container">
-            <img
-              id="purple_aster"
-              className="projects_photos"
-              src={purpleAster}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="purple_aster">Purple Aster Spec Home</label>
-          </div>
-        </Link>
+        {/* <Link to="/projects/1"> */}
+        <div className="project_container">
+          <img
+            id="purple_aster"
+            className="projects_photos"
+            src={purpleAster}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="purple_aster">Purple Aster Spec Home</label>
+        </div>
+        {/* </Link> */}
       </Tooltip>
       <Tooltip title="Coming Soon!">
-        <Link to="/projects/2">
-          <div className="project_container">
-            <img
-              id="venado_front"
-              className="projects_photos"
-              src={venadoFront}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="venado_front">Venado Spec Home</label>
-          </div>
-        </Link>
+        {/* <Link to="/projects/2"> */}
+        <div className="project_container">
+          <img
+            id="venado_front"
+            className="projects_photos"
+            src={venadoFront}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="venado_front">Venado Spec Home</label>
+        </div>
+        {/* </Link> */}
       </Tooltip>
       <Tooltip title="Coming Soon!">
-        <Link to="/projects/3">
-          <div className="project_container">
-            <img
-              id="venado_kitchen"
-              className="projects_photos"
-              src={venadoKitchen}
-              alt="project completed by Timberline Inc"
-            />
-            <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
-          </div>
-        </Link>
+        {/* <Link to="/projects/3"> */}
+        <div className="project_container">
+          <img
+            id="venado_kitchen"
+            className="projects_photos"
+            src={venadoKitchen}
+            alt="project completed by Timberline Inc"
+          />
+          <label htmlFor="venado_kitchen">Custom Kitchen Remodel</label>
+        </div>
+        {/* </Link> */}
       </Tooltip>
     </section>
   )
